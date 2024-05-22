f892f9349a7c17bce2c49a35fc3e8fa99351ba13
eldaaqollaku <eldaqollakuu@gmail.com>
2024-05-22 16:56:05 +0200

    uptade

--- src/src/main/java/Server.java
+++ src/src/main/java/Server.java
@@ -1,2 +1,37 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+import javax.crypto.Cipher;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 public class Server {
+    public static void main(String[] args) {
+        int port = 12345;
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+
+            KeyPair keyPair = KeyGenerator.generateKeyPair("Server");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                new ServerThread(socket, keyPair).start();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
